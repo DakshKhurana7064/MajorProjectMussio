cd9130b1ee71d2750603ba3c10032ac8cbf1ada4
DakshKhurana7064 <daksh7064.ca22@chitkara.edu.in>
2024-04-30 03:34:59 +0530

    mussio

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0i5OZuX9WcWs3SPMG5YDvzpsiZWGtlIU",
+  authDomain: "mussio-b753e.firebaseapp.com",
+  projectId: "mussio-b753e",
+  storageBucket: "mussio-b753e.appspot.com",
+  messagingSenderId: "1060184040834",
+  appId: "1:1060184040834:web:b9989292dca763d2f09b9b",
+  measurementId: "G-672N5M91E4"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
